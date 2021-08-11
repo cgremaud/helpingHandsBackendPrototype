1670b28299b8502cace9cd58579ffc677d0e021c
C. Gremaud <colyn.gremaud@gmail.com>
2021-08-11 08:51:30 -0500

    implemented findById in UserController

--- src/main/java/com/helpinghands/backendPrototype/UserController.java
+++ src/main/java/com/helpinghands/backendPrototype/UserController.java
@@ -1,6 +1,7 @@
 package com.helpinghands.backendPrototype;
 import com.helpinghands.backendPrototype.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,6 +17,11 @@ public class UserController {
     @GetMapping("/users")
     List<User> all() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    User one(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow();
     }
 
 }
